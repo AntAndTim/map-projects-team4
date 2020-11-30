2ba295f45c07acfc568893b532526cdcb0c4a978
Kamilya Timchenko <kamtim5@yandex.ru>
2020-11-30 17:23:20 +0300

    fix(back): Fix swagger

--- quiz_back/src/main/java/me/tim/team/quiz_back/config/CorsConfig.java
+++ quiz_back/src/main/java/me/tim/team/quiz_back/config/CorsConfig.java
@@ -2,11 +2,9 @@ package me.tim.team.quiz_back.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
