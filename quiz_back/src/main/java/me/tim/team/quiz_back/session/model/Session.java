e74437ac00276b78d23cd7eee0fa7c7bf8a31cbf
Anton Timchenko <a.timchenko@tinkoff.ru>
2020-12-14 18:40:21 +0300

    fix(QUIZ-28): fixed table names

--- quiz_back/src/main/java/me/tim/team/quiz_back/session/model/Session.java
+++ quiz_back/src/main/java/me/tim/team/quiz_back/session/model/Session.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import org.hibernate.annotations.GenericGenerator;
 
 @Data
 @Entity
+@Table(name = "QUIZ_SESSION")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Session {
